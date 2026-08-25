2d1e6d34b15c331d8aa1f759785533ccc76e928e
3chapin <46701179+3chapin@users.noreply.github.com>
2026-08-24 19:22:54 -0600

    updated main.ts to clear legacy PWA state and set favicon dynamically

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,24 @@ import App from './App.vue'
 import router from './router'
 import { setFavicon } from './utils/setFavicon'
 
+async function clearLegacyPwaState() {
+	if (!('serviceWorker' in navigator)) {
+		return
+	}
+
+	const registrations = await navigator.serviceWorker.getRegistrations()
+	await Promise.all(
+		registrations.map((registration) => registration.unregister()),
+	)
+
+	if ('caches' in window) {
+		const cacheKeys = await caches.keys()
+		await Promise.all(cacheKeys.map((key) => caches.delete(key)))
+	}
+}
+
+void clearLegacyPwaState()
+
 router.afterEach((to) => {
 	const favicon = to.meta.favicon as string | undefined
 	setFavicon(favicon ?? '/assets/neon-favicon-32.png')
